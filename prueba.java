84abbd2ab04f46548bd31df2abbf17cc49423d98
MANUEL CARLOS FERNANDEZ GUTIERREZ <0007156@viewnext.com>
2020-06-29 17:38:57 +0200

    Cambio en App Java

--- prueba.java
+++ prueba.java
@@ -1,5 +1,5 @@
 public class app {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! Lo cambio el JAva");
     }
 }git 
